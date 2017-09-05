a8298886cbaf1f0721422fdea54888eed3391143
haruka.okabayashi <mk2@mk2.red>
2017-09-06 00:01:19 +0900

    ・subscribe出来るようになった

--- app/src/main/java/mk2/red/myrxsample/activity/SimpleActivity.kt
+++ app/src/main/java/mk2/red/myrxsample/activity/SimpleActivity.kt
@@ -2,10 +2,15 @@ package mk2.red.myrxsample.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_simple.*
 import mk2.red.myrxsample.R
 import mk2.red.myrxsample.errorLog
+import mk2.red.myrxsample.model.ConnpassFeedItem
 import mk2.red.myrxsample.service.APIService
 import mk2.red.myrxsample.verboseLog
 
@@ -13,23 +18,30 @@ class SimpleActivity : AppCompatActivity() {
 
     var realm: Realm? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple)
         realm = Realm.getDefaultInstance()
 
         start_button.setOnClickListener {
-            APIService.fetchConnpassFeed({ items ->
-                verboseLog {
-                    val sb = StringBuilder()
-                    for (item in items) {
-                        sb.append(item.title + ",")
-                    }
-                    sb.toString()
-                }
-            }, { e ->
-                errorLog { e?.message!! }
-            })
+            APIService.fetchConnpassFeed().subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(object : Observer<ConnpassFeedItem> {
+                        override fun onSubscribe(d: Disposable) {
+                        }
+
+                        override fun onNext(t: ConnpassFeedItem) {
+                            verboseLog { t.link }
+                        }
+
+                        override fun onError(e: Throwable) {
+                            errorLog { e.message ?: "empty error messege" }
+                        }
+
+                        override fun onComplete() {
+                        }
+                    })
         }
     }
 }
--- app/src/main/java/mk2/red/myrxsample/service/APIService.kt
+++ app/src/main/java/mk2/red/myrxsample/service/APIService.kt
@@ -1,6 +1,7 @@
 package mk2.red.myrxsample.service
 
-import com.inaka.killertask.KillerTask
+import io.reactivex.Observable
+import mk2.red.myrxsample.errorLog
 import mk2.red.myrxsample.model.ConnpassFeedItem
 import mk2.red.myrxsample.verboseLog
 import okhttp3.OkHttpClient
@@ -14,41 +15,44 @@ object APIService {
 
     private const val ConnpassFeedUrl = "https://connpass.com/explore/ja.atom"
 
-    fun fetchConnpassFeed(onSuccess: (List<ConnpassFeedItem>) -> Unit, onFailure: (Exception?) -> Unit) {
-        val doWork: () -> List<ConnpassFeedItem> = {
-            val req = Request.Builder().url(ConnpassFeedUrl).build()
-            val res = client.newCall(req).execute()
-            val factory = XmlPullParserFactory.newInstance();
-            factory.isNamespaceAware = true
-            val parser = factory.newPullParser()
-            parser.setInput(res.body()?.byteStream(), "UTF-8")
-            var eventType = parser.eventType
-            val items = ArrayList<ConnpassFeedItem>()
-            var item = ConnpassFeedItem()
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                when (eventType) {
-                    XmlPullParser.START_DOCUMENT -> verboseLog { "Start Document" }
-                    XmlPullParser.START_TAG -> {
-                        when (parser.name) {
-                            "id" -> item.feedId = parser.nextText()
-                            "title" -> item.title = parser.nextText()
-                            "link" -> item.link = parser.getAttributeValue(null, "href")
-                            "published" -> item.published = parser.nextText()
-                            "updated" -> item.updated = parser.nextText()
-                            "summary" -> {
-                                item.summaryType = parser.getAttributeValue(null, "type")
-                                item.summary = parser.nextText()
+    fun fetchConnpassFeed(): Observable<ConnpassFeedItem> {
+        return Observable.create({ subscriber ->
+            try {
+                val req = Request.Builder().url(ConnpassFeedUrl).build()
+                val res = client.newCall(req).execute()
+                val factory = XmlPullParserFactory.newInstance();
+                factory.isNamespaceAware = true
+                val parser = factory.newPullParser()
+                parser.setInput(res.body()?.byteStream(), "UTF-8")
+                var eventType = parser.eventType
+                var item = ConnpassFeedItem()
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    when (eventType) {
+                        XmlPullParser.START_DOCUMENT -> verboseLog { "Start Document" }
+                        XmlPullParser.START_TAG -> {
+                            when (parser.name) {
+                                "id" -> item.feedId = parser.nextText()
+                                "title" -> item.title = parser.nextText()
+                                "link" -> item.link = parser.getAttributeValue(null, "href")
+                                "published" -> item.published = parser.nextText()
+                                "updated" -> item.updated = parser.nextText()
+                                "summary" -> {
+                                    item.summaryType = parser.getAttributeValue(null, "type")
+                                    item.summary = parser.nextText()
+                                }
                             }
                         }
+                        else -> verboseLog { "Unknown event type" }
                     }
-                    else -> verboseLog { "Unknown event type" }
+                    subscriber.onNext(item)
+                    item = ConnpassFeedItem()
+                    eventType = parser.next()
                 }
-                items.add(item)
-                item = ConnpassFeedItem()
-                eventType = parser.next()
+                subscriber.onComplete()
+            } catch (e: Exception) {
+                errorLog { e.toString() }
+                subscriber.onError(e)
             }
-            items
-        }
-        KillerTask(doWork, onSuccess, onFailure).go()
+        })
     }
 }
